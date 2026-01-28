8050153077240e05a63dbfc849500bbcebebf737
neel-godhaniya <godhaniyaneel7@gmail.com>
2026-01-28 14:47:28 +0530

    Update emit.js

--- emit.js
+++ emit.js
@@ -2,43 +2,64 @@ const EventEmitter = require('events');
 const eventemitter = new EventEmitter();
 
 const eventcount = {
-    login:0,
-    logout:0,
-    purchase:0,
-    prupdate:0
+    login: 0,
+    logout: 0,
+    purchase: 0,
+    prupdate: 0
 };
 
-eventemitter.on('login',(username) => {
+const users = {};
+
+eventemitter.on('login', (username) => {
     eventcount.login++;
+
+    if (!users[username]) {
+        users[username] = { purchase: {} };
+    }
+
     console.log(`${username} have logged in`);
 });
 
-eventemitter.on('logout',(username) => {
+eventemitter.on('logout', (username) => {
     eventcount.logout++;
     console.log(`${username} have logged out`);
 });
 
-eventemitter.on('purchase',(username,item) => {
+eventemitter.on('purchase', (username, item, quantity) => {
     eventcount.purchase++;
-    console.log(`${username} have purchased ${item}`);
+
+    if (!users[username]) {
+        users[username] = { purchase: {} };
+    }
+
+    users[username].purchase[item] = quantity;
+    console.log(`${username} have purchased ${item} ...${quantity}`);
 });
 
-eventemitter.on('update',(username) => {
-    eventcount.prupdate++;
-    console.log(`${username} have updated profile`);
+eventemitter.on('update', (username, item, newQuantity) => {
+    if (users[username] && users[username].purchase[item] !== undefined) {
+        users[username].purchase[item] = newQuantity;
+        console.log(`${username} updated ${item} quantity to ${newQuantity}`);
+        eventcount.prupdate++;
+    } else {
+        console.log('Invalid user or item');
+    }
 });
 
-eventemitter.on('summary',() => {
+eventemitter.on('summary', () => {
     console.log('EVENT SUMMARY');
     console.log(`number of logins - ${eventcount.login}`);
     console.log(`number of logouts - ${eventcount.logout}`);
     console.log(`number of purchases - ${eventcount.purchase}`);
     console.log(`number of profile updated - ${eventcount.prupdate}`);
-});
-eventemitter.emit('login','Neel');
-eventemitter.emit('login','aman');
-eventemitter.emit('logout','Neel');
-eventemitter.emit('purchase','aman','icecream');
-eventemitter.emit('update','aman');
-eventemitter.emit('summary');
 
+    console.log('\nUSER DATA');
+    console.log(users);
+});
+
+eventemitter.emit('login', 'Neel');
+eventemitter.emit('login', 'aman');
+eventemitter.emit('logout', 'Neel');
+eventemitter.emit('purchase', 'aman', 'icecream', 5);
+eventemitter.emit('update', 'aman', 'icecream', 10);
+eventemitter.emit('summary');
